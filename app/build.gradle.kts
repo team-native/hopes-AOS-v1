828616e92c8159d5bed82a0666ea2eb118068bf9
cnsvkf <ttagno4230@gmail.com>
2026-09-13 18:50:10 +0900

    chore(release): prepare version 1.0.2
    Related issue: #213

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.teamnative.hopes"
         minSdk = 34
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
